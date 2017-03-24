5db3d88d2ec12f3fda3a92b0efe8406c6f5e15a6
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-03-24 16:17:06 -0400

    started setting up permission system schema

---
+++ store/pg/migrations/v12_schema.go
@@ -0,0 +1,49 @@
+package migrations
+
+const setupRoleBasedPermissions = `
+DROP TABLE permissions;
+
+CREATE TABLE roles(
+       id SERIAL PRIMARY KEY,
+       name varchar(100) NOT NULL
+);
+
+CREATE TABLE user_roles(
+       user_id    integer REFERENCES users(id),
+       project_id integer REFERENCES projects(id),
+       role_id    integer REFERENCES roles(id)
+);
+
+INSERT INTO roles (name) VALUES ('Admin');
+INSERT INTO roles (name) VALUES ('Contributor');
+INSERT INTO roles (name) VALUES ('User');
+INSERT INTO roles (name) VALUES ('Anonymous');
+
+CREATE TABLE permissions (
+       id SERIAL PRIMARY KEY,
+       name varchar(100)
+);
+
+INSERT INTO permissions (name) VALUES ('VIEW_PROJECT');
+INSERT INTO permissions (name) VALUES ('CREATE_TICKET');
+INSERT INTO permissions (name) VALUES ('COMMENT_TICKET');
+
+CREATE TABLE permission_schemes(
+       id SERIAL PRIMARY KEY,
+       name varchar(100),
+       description varchar(250),
+);
+
+CREATE TABLE permission_scheme_permissions(
+       role_id integer REFERENCES roles(id),
+       scheme_id integer REFERENCES permission_schemes(id),
+       perm_id integer REFERENCES permissions(id)
+);
+
+CREATE TABLE project_permission_schemes(
+       permission_scheme_id integer REFERENCES permission_schemes(id),
+       project_id           integer REFERENCES projects(id)
+);
+`
+
+var v12schema = schema{12, setupRoleBasedPermissions, "setup role based permissions"}
