e8f642a895d033c89b230ae8684d35618b7d9696
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-03-28 14:49:53 -0400

    rewrite models for managing permissions to match new schema

--- models/permission.go
+++ models/permission.go
@@ -1,49 +1,20 @@
 package models
 
-import (
-	"time"
-)
-
-// PermissionLevel represents a permission level.
-type PermissionLevel int
-
-// Permission Levels
-const (
-	Admin       PermissionLevel = 1
-	Contributor                 = 2
-	Viewer                      = 3
-)
-
-func (pl PermissionLevel) String() string {
-	switch pl {
-	case 1:
-		return "ADMIN"
-	case 2:
-		return "CONTRIBUTOR"
-	case 3:
-		return "VIEWER"
-	default:
-		return "ANONYMOUS"
-	}
+// Role represents a role on a project, the defaults are
+// Administrator, Contributor, User, and Anonymous these are user
+// configurable. If members is present this means you are looking at
+// that role for a given project.
+type Role struct {
+	ID      int64    `json:"id"`
+	Name    string   `json:"name"`
+	Project *Project `json:"project,omitempty"`
+	Members []User   `json:"members,omitempty"`
 }
 
-// MarshalJSON implements json.Marshaler for our PermissionLevel this
-// allows us to store "machine readable" permission levels but give
-// "human readable" permission levels to the clients.
-func (pl PermissionLevel) MarshalJSON() ([]byte, error) {
-	return []byte("\"" + pl.String() + "\""), nil
-}
-
-// Permission is used to control user / team access to projects.
-type Permission struct {
-	ID          int64           `json:"id"`
-	CreatedDate time.Time       `json:"created_date"`
-	UpdatedDate time.Time       `json:"updated_date"`
-	Level       PermissionLevel `json:"level"`
-	User        User            `json:"user,omitempty"`
-	Team        Team            `json:"team,omitempty"`
-}
-
-func (p *Permission) String() string {
-	return jsonString(p)
+// PermissionScheme is used to map roles to permissions
+type PermissionScheme struct {
+	ID          int64               `json:"id"`
+	Name        string              `json:"name"`
+	Description string              `json:"description"`
+	Permissions map[string][]string `json:"permissions"`
 }
