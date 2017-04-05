35e1399a7c669cf3250273cc6fff22c6eccb59cc
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-04-05 14:39:13 -0400

    update models for permissions and schemes

--- models/permission.go
+++ models/permission.go
@@ -1,5 +1,7 @@
 package models
 
+import "github.com/praelatus/praelatus/models/permission"
+
 // Role represents a role on a project, the defaults are
 // Administrator, Contributor, User, and Anonymous these are user
 // configurable. If members is present this means you are looking at
@@ -11,10 +13,24 @@ type Role struct {
 	Members []User   `json:"members,omitempty"`
 }
 
+func (r Role) String() string {
+	return jsonString(r)
+}
+
 // PermissionScheme is used to map roles to permissions
 type PermissionScheme struct {
-	ID          int64               `json:"id"`
-	Name        string              `json:"name"`
-	Description string              `json:"description"`
-	Permissions map[string][]string `json:"permissions"`
+	ID          int64                              `json:"id"`
+	Name        string                             `json:"name"`
+	Description string                             `json:"description"`
+	Permissions map[string][]permission.Permission `json:"permissions"`
+}
+
+func (p PermissionScheme) String() string {
+	return jsonString(p)
+}
+
+// Permission is a permission in the DB
+type Permission struct {
+	ID   int64                 `json:"id"`
+	Name permission.Permission `json:"name"`
 }
