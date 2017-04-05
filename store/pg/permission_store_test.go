e1b897875e9cafbbfa6dde1113ee412efd88b57f
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-04-05 15:31:01 -0400

    stub out tests for permission store

---
+++ store/pg/permission_store_test.go
@@ -0,0 +1,29 @@
+package pg_test
+
+import "testing"
+
+// TODO write tests
+func TestPermissionStoreGet(t *testing.T) {
+	t.Fail()
+	return
+}
+
+func TestPermissionStoreGetAll(t *testing.T) {
+	t.Fail()
+	return
+}
+
+func TestPermissionStoreCreate(t *testing.T) {
+	t.Fail()
+	return
+}
+
+func TestPermissionStoreSave(t *testing.T) {
+	t.Fail()
+	return
+}
+
+func TestPermissionStoreRemove(t *testing.T) {
+	t.Fail()
+	return
+}
