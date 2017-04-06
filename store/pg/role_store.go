7f4919c98c4be71c2057fb1018db1bb8d9fd6be0
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-04-06 13:52:49 -0400

    finish writing role store code

--- store/pg/role_store.go
+++ store/pg/role_store.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 
 	"github.com/praelatus/praelatus/models"
+	"github.com/praelatus/praelatus/models/permission"
 	"github.com/praelatus/praelatus/store"
 )
 
@@ -13,24 +14,48 @@ type RoleStore struct {
 	db *sql.DB
 }
 
-func (rs *RoleStore) Get(u models.User, r *models.Role) error {
-	if !checkIfAdmin(rs.db, u.ID) {
-		return store.ErrPermissionDenied
-	}
+func (rs *RoleStore) Get(r *models.Role) error {
+	err := rs.db.
+		QueryRow("SELECT id, name FROM roles WHERE id = $1 OR name = $2",
+			r.ID, r.Name).
+		Scan(&r.ID, &r.Name)
 
-	return nil
+	return handlePqErr(err)
 }
 
-func (rs *RoleStore) GetAll(u models.User) ([]models.Role, error) {
-	if !checkIfAdmin(rs.db, u.ID) {
-		return nil, store.ErrPermissionDenied
+// GetAll will return all roles for this instance of Praelatus
+func (rs *RoleStore) GetAll() ([]models.Role, error) {
+	rows, err := rs.db.Query("SELECT id, name FROM roles")
+	if err != nil {
+		return nil, handlePqErr(err)
 	}
 
-	return nil, nil
+	var roles []models.Role
+
+	for rows.Next() {
+		var r models.Role
+
+		err = rows.Scan(&r.ID, &r.Name)
+		if err != nil {
+			return roles, handlePqErr(err)
+		}
+
+		roles = append(roles, r)
+	}
+
+	return roles, nil
 }
 
 func (rs *RoleStore) New(r *models.Role) error {
-	return nil
+	err := rs.db.QueryRow(`
+INSERT INTO roles (name) 
+VALUES ($1)
+RETURNING id
+`,
+		r.Name).
+		Scan(&r.ID)
+
+	return handlePqErr(err)
 }
 
 func (rs *RoleStore) Create(u models.User, r *models.Role) error {
@@ -46,7 +71,14 @@ func (rs *RoleStore) Save(u models.User, r models.Role) error {
 		return store.ErrPermissionDenied
 	}
 
-	return nil
+	_, err := rs.db.Exec(`
+UPDATE roles 
+SET (name) = ($2)
+WHERE id = $1
+`,
+		r.ID, r.Name)
+
+	return handlePqErr(err)
 }
 
 func (rs *RoleStore) Remove(u models.User, r models.Role) error {
@@ -54,22 +86,78 @@ func (rs *RoleStore) Remove(u models.User, r models.Role) error {
 		return store.ErrPermissionDenied
 	}
 
-	return nil
+	tx, err := rs.db.Begin()
+	if err != nil {
+		return handlePqErr(err)
+	}
+
+	_, err = tx.Exec("DELETE FROM user_roles WHERE role_id = $1",
+		r.ID)
+	if err != nil {
+		tx.Rollback()
+		return handlePqErr(err)
+	}
+
+	_, err = tx.Exec(`
+DELETE FROM permission_scheme_permissions WHERE role_id = $1
+`,
+		r.ID)
+	if err != nil {
+		tx.Rollback()
+		return handlePqErr(err)
+	}
+
+	_, err = tx.Exec("DELETE FROM roles WHERE id = $1",
+		r.ID)
+	if err != nil {
+		tx.Rollback()
+		return handlePqErr(err)
+	}
+
+	return handlePqErr(tx.Commit())
 }
 
 func (rs *RoleStore) AddUserToRole(userAdding models.User, userToAdd models.User,
 	project models.Project, role models.Role) error {
-	if !checkIfAdmin(rs.db, userAdding.ID) {
+	if !checkPermission(rs.db, permission.ADMINPROJECT, project.ID, userAdding.ID) && !checkIfAdmin(rs.db, userAdding.ID) {
 		return store.ErrPermissionDenied
 	}
 
-	return nil
+	_, err := rs.db.Exec(`
+INSERT INTO user_roles (user_id, project_id, role_id)
+VALUES ($1, $2, $3)
+`,
+		userToAdd.ID, project.ID, role.ID)
+
+	return handlePqErr(err)
 }
 
 func (rs *RoleStore) GetForUser(u models.User) ([]models.Role, error) {
-	if !checkIfAdmin(rs.db, u.ID) {
-		return nil, store.ErrPermissionDenied
+	var roles []models.Role
+
+	rows, err := rs.db.Query(`
+SELECT r.id, r.name FROM roles AS r
+JOIN user_roles AS ur ON ur.role_id = r.id
+JOIN users AS u ON u.id = ur.user_id
+JOIN projects AS p ON p.id = ur.project_id
+WHERE u.id = $1
+`,
+		u.ID)
+
+	if err != nil {
+		return roles, handlePqErr(err)
 	}
 
-	return nil, nil
+	for rows.Next() {
+		var r models.Role
+
+		err := rows.Scan(&r.ID, &r.Name)
+		if err != nil {
+			return roles, handlePqErr(err)
+		}
+
+		roles = append(roles, r)
+	}
+
+	return roles, nil
 }
