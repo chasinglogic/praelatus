eb706f77796765f262391c8897590edf692c465f
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-04-10 11:00:32 -0400

    make models importable

---
+++ praelatus/models/__init__.py
@@ -0,0 +1,7 @@
+from praelatus.models.base import Base
+from praelatus.models.users import User
+from praelatus.models.projects import Project
+from praelatus.models.permissions import Role, Permission, PermissionScheme
+from praelatus.models.fields import Field, FieldOption, FieldValue
+from praelatus.models.workflows import Workflow, Transition, Hook
+from praelatus.models.tickets import Ticket, Comment, TicketType, Label
