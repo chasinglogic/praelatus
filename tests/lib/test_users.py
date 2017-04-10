a8c8eaee55049b646f09a6e5fe9453430bb76242
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-04-10 19:42:03 -0400

    remove erroneous code

--- tests/lib/test_users.py
+++ tests/lib/test_users.py
@@ -9,15 +9,6 @@ def db():
 
 
 def test_new_and_get(db):
-    # users.new(
-    #     db,
-    #     username='testadmin',
-    #     password='test',
-    #     email='test@example.com',
-    #     is_admin=True,
-    #     full_name='Test Testerson'
-    # )
-
     user = users.get(db, username='testadmin')
     assert user is not None
     assert user.username == 'testadmin'
