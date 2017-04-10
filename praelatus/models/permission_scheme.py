462f8855ae6ce070f192380cef8802cca5985b12
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-04-10 09:24:34 -0400

    finish up permission scheme and roles

--- praelatus/models/permission_scheme.py
+++ praelatus/models/permission_scheme.py
@@ -1,9 +1,23 @@
 from base import Base
+from sqlalchemy import Column, Integer, String, relationship
 
-# TODO handle permissions
 class PermissionScheme(Base):
     __tablename__ = 'permission_schemes'
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
     description = Column(String)
+
+
+class Permission(Base):
+    __tablename__ = 'permissions'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+
+class Role(Base):
+    __tablename__ = 'roles'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
