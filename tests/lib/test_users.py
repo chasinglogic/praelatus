91471557fc0987de4ab188b0744ea58afae259d2
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-04-11 15:53:56 -0400

    rename tests

--- tests/lib/test_users.py
+++ tests/lib/test_users.py
@@ -8,11 +8,12 @@ def db():
     return session()
 
 
-def test_new_and_get(db):
+def test_get(db):
     user = users.get(db, username='testadmin')
     assert user is not None
     assert user.username == 'testadmin'
 
+def test_get_with_filter(db):
     usrs = users.get(db, filter='test*')
     assert usrs is not None
     assert len(usrs) > 0
