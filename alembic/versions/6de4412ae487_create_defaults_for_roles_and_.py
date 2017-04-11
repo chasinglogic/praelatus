35140456ee3b3bc11a21618f2a1e8d782babc764
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-04-11 15:54:02 -0400

    start custom migration to add roles and permission to database

---
+++ alembic/versions/6de4412ae487_create_defaults_for_roles_and_.py
@@ -0,0 +1,24 @@
+"""Create defaults for roles and permissions
+
+Revision ID: 6de4412ae487
+Revises: 720fb4e00eff
+Create Date: 2017-04-11 15:51:52.705462
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6de4412ae487'
+down_revision = '720fb4e00eff'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
