6c4f71f8d3ca2a4d2f44f9547bb3fe0b92fdd1b2
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-04-12 17:14:47 -0400

    define entrypoints

--- setup.py
+++ setup.py
@@ -3,5 +3,10 @@ from setuptools import setup
 setup(
     name='praelatus',
     packages=['praelatus'],
-    install_requires=open('./requirements.txt').split('\n')
+    install_requires=open('./requirements.txt').read().split('\n'),
+    entry_points={
+        'console_scripts': [
+            'praelatus = praelatus.cli:cli'
+        ]
+    }
 )
