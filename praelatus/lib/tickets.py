527a4fb7f48b64306cba298e7b93a5fae6dd87da
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-04-13 14:13:56 -0400

    add docstrings

--- praelatus/lib/tickets.py
+++ praelatus/lib/tickets.py
@@ -1,11 +1,43 @@
+"""
+Contains methods for interacting with tickets.
+
+Anywhere a db is taken it is assumed to be a sqlalchemy session
+created by a SessionMaker instance.
+
+Anywhere actioning_user is a keyword argument, this is the user
+performing the call and the permissions of the provided user will be
+checked before committing the action. None is equivalent to an
+Anonymous user.
+"""
+
 from sqlalchemy import or_
 from sqlalchemy.orm import joinedload
-from praelatus.models import (Ticket, User, Label, FieldValue, Status,
-                              Comment, TicketType)
+from praelatus.models import Ticket
+from praelatus.models import User
+from praelatus.models import Label
+from praelatus.models import FieldValue
+from praelatus.models import Status
+from praelatus.models import Comment
+from praelatus.models import TicketType
 
 
 def get(db, id=None, key=None, reporter=None, assignee=None,
         filter=None, actioning_user=None):
+    """
+    Get a ticket from the database.
+
+    If the keyword arguments id or key are specified returns a single
+    sqlalchemy result, otherwise returns all matching results.
+
+    keyword arguments:
+    actioning_user -- the user requesting the ticket (default None)
+    id -- database id (default None)
+    key -- the ticket key i.e. TEST-123 (default None)
+    reporter -- User class instance who is the reporter (default None)
+    assignee -- User class instance who is the assignee (default None)
+    filter -- a pattern to search through tickets with (default None)
+    """
+
     query = db.query(Ticket).join(
         FieldValue,
         Label,
@@ -44,3 +76,6 @@ def get(db, id=None, key=None, reporter=None, assignee=None,
     if any([id, key]):
         return query.first()
     return query.order_by(Ticket.key).all()
+
+
+def new(db, **kwargs):
