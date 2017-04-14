460184b8f7b6d70b6a192eca7e82d4c7f679b189
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-04-14 13:21:44 -0400

    add doc string and version info

---
+++ praelatus/__init__.py
@@ -0,0 +1,11 @@
+"""
+Praelatus is an Open Source bug tracking and ticketing system.
+
+The backend API is written in Python and the frontend is a React.js
+app. You are viewing the source code for the API if you would like
+information about how to use the API as a client or how to start
+working on the backend visit https://docs.praelatus.io
+"""
+
+__version__ = 'v0.0.4'
+__api_version__ = 'v1'
