e6c0ce919683ebe7fcf252108af076626a3ed9d1
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-04-14 13:26:44 -0400

    update docs and add name to get

--- praelatus/lib/projects.py
+++ praelatus/lib/projects.py
@@ -1,4 +1,15 @@
-""""""
+"""
+Contains methods for interacting with projects.
+
+Anywhere a db is taken it is assumed to be a sqlalchemy session
+created by a SessionMaker instance.
+
+Anywhere actioning_user is a keyword argument, this is the user
+performing the call and the permissions of the provided user will be
+checked before committing the action. None is equivalent to an
+Anonymous user.
+"""
+
 from praelatus.lib.utils import rollback
 from praelatus.lib.permissions import permission_required, add_permission_query
 from praelatus.models.projects import Project
@@ -6,10 +17,21 @@ from praelatus.models.users import User
 from sqlalchemy import or_
 
 
-def get(db, key=None, id=None, filter=None, actioning_user=None):
-    """get gets projects from the database, if key or id are given one
-    project is returned otherwise if filter is specified it will
-    return all projects which match the given filter."""
+def get(db, key=None, id=None, name=None, filter=None, actioning_user=None):
+    """
+    Get projects from the database.
+
+    If the keyword arguments id, name, or key are specified returns a
+    single sqlalchemy result, otherwise returns all matching results.
+
+    keyword arguments:
+    actioning_user -- the user requesting the project (default None)
+    id -- database id (default None)
+    key -- the project key (default None)
+    name -- the project name (default None)
+    filter -- a pattern to search through projects with (default None)
+    """
+
     query = db.query(Project).join(User)
 
     if key is not None:
@@ -17,6 +39,9 @@ def get(db, key=None, id=None, filter=None, actioning_user=None):
 
     if id is not None:
         query = query.filter(Project.id == id)
+
+    if name is not None:
+        query = query.filter(Project.name == name)
 
     if filter is not None:
         pattern = filter.replace('*', '%')
@@ -38,34 +63,46 @@ def get(db, key=None, id=None, filter=None, actioning_user=None):
 
 @rollback
 def new(db, **kwargs):
-    """new will create a new project in the database"""
-    try:
-        lead = kwargs.get('lead')
-        permission_scheme = kwargs.get('permission_scheme')
+    """
+    Creates a new project in the database; then returns that project.
 
-        new_project = Project(
-                key=kwargs['key'],
-                homepage=kwargs.get('homepage'),
-                repo=kwargs.get('repo'),
-                icon_url=kwargs.get('icon_url'),
-                name=kwargs['name']
-        )
+    The kwargs are parsed such that if a json representation of a
+    project is provided as expanded kwargs it will be handled
+    properly.
 
-        if lead is not None:
-            new_project.lead_id = lead['id']
+    If a required argument is not provided then it raises a KeyError
+    indicating which key was missing. Useful for returning HTTP 400
+    errors.
 
-        permission_scheme = kwargs.get('permission_scheme', None)
-        if permission_scheme is None:
-            # The default permission scheme should always be id 1
-            new_project.permission_scheme_id = 1
-        else:
-            new_project.perimssion_scheme_id = permission_scheme['id']
+    required keyword arguments:
+    key -- the project key
+    name -- the project name
 
-        db.add(new_project)
-        db.commit()
-        return new_project
-    except KeyError as e:
-        raise Exception('Missing key ' + str(e.args[0]))
+    optional keyword arguments
+    homepage -- the homepage for the project
+    repo -- the repository for this projects code
+    permission_scheme -- the permission scheme to use for this project
+    lead -- the project lead for this project
+    """
+
+    new_project = Project(
+        key=kwargs['key'],
+        homepage=kwargs.get('homepage'),
+        repo=kwargs.get('repo'),
+        icon_url=kwargs.get('icon_url'),
+        name=kwargs['name']
+    )
+
+    lead = kwargs.get('lead')
+    if lead is not None:
+        new_project.lead_id = lead['id']
+
+    permission_scheme = kwargs.get('permission_scheme', {})
+    new_project.permission_scheme_id = permission_scheme.get('id', 1)
+
+    db.add(new_project)
+    db.commit()
+    return new_project
 
 
 @rollback
