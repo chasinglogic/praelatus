b735f37c703af59118f119786e2f59928dbbc8e6
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-04-14 13:26:38 -0400

    add docs

--- praelatus/lib/labels.py
+++ praelatus/lib/labels.py
@@ -1,9 +1,36 @@
+"""
+Contains methods for interacting with labels.
+
+Anywhere a db is taken it is assumed to be a sqlalchemy session
+created by a SessionMaker instance.
+
+Anywhere actioning_user is a keyword argument, this is the user
+performing the call and the permissions of the provided user will be
+checked before committing the action. None is equivalent to an
+Anonymous user.
+"""
+
 from praelatus.models import Label, Ticket
 from praelatus.lib.utils import rollback
 from sqlalchemy.orm import joinedload
 
 
 def get(db, id=None, name=None, filter=None, preload_tickets=False):
+
+    """
+    Get projects from the database.
+
+    If the keyword arguments id, name, or key are specified returns a
+    single sqlalchemy result, otherwise returns all matching results.
+
+    keyword arguments:
+    actioning_user -- the user requesting the project (default None)
+    id -- database id (default None)
+    key -- the project key (default None)
+    name -- the project name (default None)
+    filter -- a pattern to search through projects with (default None)
+    """
+
     query = db.query(Label)
 
     if id is not None:
