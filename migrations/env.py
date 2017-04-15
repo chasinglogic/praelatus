9e0cbca94a5ff98d499aac456763718c5b9a991e
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-04-15 12:03:04 -0400

    make alembic named migrations so we can include it in egg

--- migrations/env.py
+++ migrations/env.py
@@ -1,9 +1,9 @@
+# noqa: D100
 from __future__ import with_statement
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
 from praelatus.models import Base
-from praelatus.config import config as app_config
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -25,7 +25,8 @@ target_metadata = Base.metadata
 
 def get_url():
     """Get the database url using the baked in Praelatus method."""
-    return app_config['DB_URL']
+    from praelatus.config import config as app_config
+    return app_config.db_url
 
 
 def run_migrations_offline():
@@ -40,9 +41,10 @@ def run_migrations_offline():
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
-    context.configure(
-        url=url, target_metadata=target_metadata, literal_binds=True)
+    url = get_url()
+    context.configure(url=url,
+                      target_metadata=target_metadata,
+                      literal_binds=True)
 
     with context.begin_transaction():
         context.run_migrations()
@@ -57,6 +59,7 @@ def run_migrations_online():
     """
     connectable = engine_from_config(
         config.get_section(config.config_ini_section),
+        url=get_url(),
         prefix='sqlalchemy.',
         poolclass=pool.NullPool)
 
@@ -68,6 +71,7 @@ def run_migrations_online():
 
         with context.begin_transaction():
             context.run_migrations()
+
 
 if context.is_offline_mode():
     run_migrations_offline()
