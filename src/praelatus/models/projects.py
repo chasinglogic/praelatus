2955fcb043350bb9d16205bf079542531b4b2dff
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-04-16 13:56:29 -0400

    create ProjectRoles for easier jsonizing of UserRoles

--- src/praelatus/models/projects.py
+++ src/praelatus/models/projects.py
@@ -1,11 +1,16 @@
-from praelatus.models.base import Base
+"""Contains definitions for the Project and ProjectRoles classes."""
+
+import json
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 
+from praelatus.models.base import Base
 
-# TODO add roles as a through relationship
+
 class Project(Base):
+    """Defines a Project in the database."""
+
     __tablename__ = 'projects'
 
     id = Column(Integer, primary_key=True)
@@ -24,4 +29,31 @@ class Project(Base):
     permission_scheme = relationship('PermissionScheme', backref='projects')
 
     def __repr__(self):
+        """Stringify the project for prettier printing."""
         return "Project(id=%d, key=%s)" % (self.id, self.key)
+
+
+class ProjectRoles:
+    """Converts UserRoles instances into the correct JSON."""
+
+    def __init__(self, user_roles):
+        """Take an array of user_roles and parse appropriately."""
+        self.roles = {}
+        for r in user_roles:
+            self.add_role(r)
+        return self
+
+    @staticmethod
+    def parse(user_role):
+        """Parse out the role name and user as JSON for user_role."""
+        return (user_role.role.name, user_role.user.to_json())
+
+    def add_role(self, user_role):
+        """Take a user_role parse then store in self.roles."""
+        role_name, user = self.parse(user_role)
+        members = self.roles.get(role_name, [])
+        self.roles[role_name] = members.append(user)
+
+    def to_json(self):
+        """Return the json of self.roles."""
+        return json.dumps(self.roles)
