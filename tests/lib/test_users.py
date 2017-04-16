b6b60bcaf947b90353fef3139cbb9997b9c1cf5a
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-04-16 15:32:47 -0400

    finish user lib tests

--- tests/lib/test_users.py
+++ tests/lib/test_users.py
@@ -6,8 +6,46 @@ def test_get(db):
     assert user is not None
     assert user.username == 'testadmin'
 
+
 def test_get_with_filter(db):
     usrs = users.get(db, filter='test*')
     assert usrs is not None
     assert len(usrs) > 0
     assert usrs[0].username == 'testadmin'
+
+
+def test_delete(db):
+    users.new(db, **{
+        'username': 'delete_me',
+        'password': 'none',
+        'email': 'delete_me@delete.com',
+        'full_name': 'DELETE ME',
+        'is_active': True,
+    })
+
+    # verify we can retreieve the user
+    u = users.get(db, username='delete_me')
+    assert u is not None
+
+    users.delete(db, u, actioning_user=u)
+
+    u = users.get(db, username='delete_me')
+    assert u is None
+
+
+def test_update(db):
+    email = 'anonmyous@example.com'
+    anon = users.get(db, id=1)
+
+    anon.email = email
+
+    users.update(db, anon, actioning_user=anon)
+
+    a = users.get(db, id=1)
+    assert a is not None
+    assert a.email == email
+
+
+def test_gravatar():
+    gravatar = 'https://gravatar.com/avatar/9cd743073d56a5958655e3cf2105728b'
+    assert gravatar == users.gravatar('chasinglogic@gmail.com')
