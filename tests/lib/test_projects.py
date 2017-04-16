874fb5eab9e84929b9d7c5ced0b87b759262b7da
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-04-16 18:20:48 -0400

    finish projects tests

--- tests/lib/test_projects.py
+++ tests/lib/test_projects.py
@@ -27,4 +27,32 @@ def test_get_filter_action(db):
 
 
 def test_update(db):
-    admin = users.get(db, username='testadmin')
+    new_name = 'Super Duper Test Project'
+    lead = users.get(db, username='testuser')
+    project = projects.get(db, key='TEST')
+    project.name = new_name
+
+    projects.update(db, project=project, actioning_user=lead)
+
+    proj = projects.get(db, id=project.id)
+    assert proj is not None
+    assert proj.name == new_name
+
+
+def test_delete(db):
+    lead = users.get(db, username='testuser')
+    project = {
+            'name': 'DELETE THIS PROJECT',
+            'key':  'DELETE',
+            'lead': {'id': lead.id},
+    }
+
+    projects.new(db, **project)
+
+    p = projects.get(db, key='DELETE')
+    assert p is not None
+
+    projects.delete(db, project=p, actioning_user=lead)
+
+    p = projects.get(db, key='DELETE')
+    assert p is None
