db411a564ac924d707906b780a900eac413e56d4
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-04-16 14:35:08 -0400

    make a BaseModel class to share functionality

--- src/praelatus/models/base.py
+++ src/praelatus/models/base.py
@@ -1,3 +1,47 @@
+"""Contains the Base model which all models inherit from."""
+
+import json
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
+
+class BaseModel:
+    """The Base for all models in Praelatus."""
+
+    # This is so the __repr__ never throws AttributeError
+    id = None
+
+    def __repr__(self):
+        """A basic repr method that can be inherited."""
+        return "%s(id=%s)" % (self.__class__.__name__, self.id)
+
+    def clean_dict(self):
+        """
+        Return a dictionary of the obj with metadata removed.
+
+        This is used for easy jsonifying of models.
+        """
+        # This stores the final json obj
+        jsn = {}
+
+        for key, val in self.__dict__.items():
+            # Check if it's metadata, if so skip it.
+            if key.startswith("_"):
+                continue
+
+            # Check if it's a model, if so jsonify it first.
+            if isinstance(val, Base):
+                jsn[key] = val.clean_dict()
+                continue
+
+            # Must be a primitive type so just throw it in.
+            jsn[key] = val
+
+        return jsn
+
+    def to_json(self):
+        """A basic to_json method that works for 90% of the models."""
+        # jsonify our dict using the stdlib
+        return json.dumps(self.clean_dict())
+
+
+Base = declarative_base(cls=BaseModel)
