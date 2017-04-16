46921bdd76fd5df29b8b83ceae93d3cdeb113fbd
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-04-16 19:26:11 -0400

    add seeding

--- tests/lib/conftest.py
+++ tests/lib/conftest.py
@@ -1,10 +1,26 @@
-# noqa: D100
+# flake8: noqa
 
 import pytest
 from praelatus.lib import session
+from praelatus.seeds import seed
+import praelatus.lib.users as users
+
+global seeded
+seeded = False
+
+@pytest.fixture(scope='module')
+def admin():
+    return users.get(session(), username='testadmin')
 
 
 @pytest.fixture(scope='module')
 def db():
     """Return a database session as a fixture."""
+    sess = session()
+
+    if not seeded:
+        seed(sess)
+        global seeded
+        seeded = True
+
     return session()
