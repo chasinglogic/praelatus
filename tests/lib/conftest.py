feaaa06b8ffb16acd04cb88213794c5f2c5f3591
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-04-16 21:21:29 -0400

    make work with python 3.6

--- tests/lib/conftest.py
+++ tests/lib/conftest.py
@@ -5,9 +5,6 @@ from praelatus.lib import session
 from praelatus.seeds import seed
 import praelatus.lib.users as users
 
-global seeded
-seeded = False
-
 @pytest.fixture(scope='module')
 def admin():
     return users.get(session(), username='testadmin')
@@ -16,11 +13,4 @@ def admin():
 @pytest.fixture(scope='module')
 def db():
     """Return a database session as a fixture."""
-    sess = session()
-
-    if not seeded:
-        seed(sess)
-        global seeded
-        seeded = True
-
     return session()
