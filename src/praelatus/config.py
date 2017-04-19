eb474617a70fc40a1f2733f0a44b5ac8c7be6d30
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-04-18 21:47:15 -0400

    add redis to config and json serialization

--- src/praelatus/config.py
+++ src/praelatus/config.py
@@ -8,6 +8,9 @@ class Config:
     """Contains all global app configuration and defaults."""
 
     default_db = 'postgres://postgres:postgres@localhost:5432/prae_dev'
+    default_redis_host = 'localhost'
+    default_redis_port = 6379
+    default_redis_db = 0
     default_port = '8080'
     default_data_dir = '/var/praelatus/data'
 
@@ -15,13 +18,19 @@ class Config:
         """Build a new config."""
         self.db_url = kwargs.get('db_url', self.default_db)
         self.port = kwargs.get('port', self.default_port)
-        self.redis_url = kwargs.get('redis_url')
+        self.redis_host = kwargs.get('redis_url', self.default_redis_host)
+        self.redis_port = int(kwargs.get('redis_port', self.default_redis_port))  # noqa
+        self.redis_db = int(kwargs.get('redis_db', self.default_redis_db))
         self.redis_password = kwargs.get('redis_password')
         self.data_dir = kwargs.get('data_dir')
 
     def __repr__(self):
         """Return the str version of the internal dict."""
         return str(self.__dict__)
+
+    def to_json(self):
+        """Serialize the config to json."""
+        return json.dumps(self.__dict__)
 
     def load(self):
         """Create a new Config based on the config file or environment variables."""  # noqa: E501
