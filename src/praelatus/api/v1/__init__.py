491668670c3518df09ef876fb8a195350fc35bb3
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-04-19 17:07:01 -0400

    add create_session ability to UsersResource

--- src/praelatus/api/v1/__init__.py
+++ src/praelatus/api/v1/__init__.py
@@ -2,9 +2,11 @@
 
 from praelatus.api.v1.users import UsersResource
 from praelatus.api.v1.users import UserResource
+from praelatus.api.v1.users import SessionResource
 
 
 def add_v1_routes(app, prefix='/api/v1/'):
     """Add all of the v1 routes to the given app."""
-    app.add_route(prefix+'users', UsersResource())
+    app.add_route(prefix+'users', UsersResource(SessionResource.create_session))
     app.add_route(prefix+'users/{username}', UserResource())
+    app.add_route(prefix+'users/sessions', SessionResource())
