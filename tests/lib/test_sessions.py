ba083cc9a8157bd41eb00d21b8c4ddf49b6ca7df
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-04-18 22:36:45 -0400

    add delete to test

--- tests/lib/test_sessions.py
+++ tests/lib/test_sessions.py
@@ -1,8 +1,12 @@
 import praelatus.lib.sessions as sessions
 
 
-def test_set_and_get(admin):
+def test_set_delete_and_get(admin):
     sess_id = sessions.gen_session_id()
     sessions.set(sess_id, admin)
     u = sessions.get(sess_id)
     assert str(u) == str(admin)
+
+    sessions.delete(sess_id)
+    u = sessions.get(sess_id)
+    assert u is None
