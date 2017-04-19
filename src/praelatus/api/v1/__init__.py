fb6696ec7d7f791bd03badc5b10ea2b1debe4fa9
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-04-18 21:47:05 -0400

    add v1 routes

--- src/praelatus/api/v1/__init__.py
+++ src/praelatus/api/v1/__init__.py
@@ -1,1 +1,10 @@
 """Contains the API Resources for API V1."""
+
+from praelatus.api.v1.users import UsersResource
+from praelatus.api.v1.users import UserResource
+
+
+def add_v1_routes(app, prefix='/api/v1/'):
+    """Add all of the v1 routes to the given app."""
+    app.add_route(prefix+'users', UsersResource())
+    app.add_route(prefix+'users/{username}', UserResource())
