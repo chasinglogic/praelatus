efcdbefb41d90c871634ba861ab95b3ca5839354
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-04-18 22:35:22 -0400

    write session endpoints

--- src/praelatus/api/v1/users.py
+++ src/praelatus/api/v1/users.py
@@ -3,10 +3,14 @@
 import falcon
 import json
 
+from datetime import datetime
+from datetime import timedelta
+
 import praelatus.lib.users as users
+import praelatus.lib.sessions as sessions
 
 from praelatus.models import User
-from praelatus.lib import session
+from praelatus.lib import session as db
 
 
 class UsersResource():
@@ -17,7 +21,7 @@ class UsersResource():
         user = req.context.get('user', {})
         query = req.params.get('filter', '*')
 
-        db_res = users.get(session(), filter=query,
+        db_res = users.get(db(), filter=query,
                            actioning_user=user)
         usrs = []
         for u in db_res:
@@ -29,7 +33,7 @@ class UsersResource():
     def on_post(self, req, resp):
         """Create a user."""
         user = json.load(req.stream)
-        db_u = users.new(session(), **user)
+        db_u = users.new(db(), **user)
         resp.body = json.dumps(db_u)
         resp.status = falcon.HTTP_200
 
@@ -40,7 +44,7 @@ class UserResource():
     def on_get(self, req, resp, username):
         """Return single user by username or id."""
         user = req.context.get('user', {})
-        db_res = users.get(session(), username=username,
+        db_res = users.get(db(), username=username,
                            actioning_user=user)
         resp.body = db_res.to_json()
         resp.status = falcon.HTTP_200
@@ -50,13 +54,13 @@ class UserResource():
         jsn = json.load(req.stream)
         new_u = User.from_json(jsn)
         user = req.context.get('user', {})
-        users.update(session(), actioning_user=user, user=new_u)
+        users.update(db(), actioning_user=user, user=new_u)
         resp.body = json.dumps({'message': 'Successfully updated user.'})
         resp.status = falcon.HTTP_200
 
     def on_delete(self, req, resp, username):
         """Update user."""
-        sess = session()
+        sess = db()
         user = req.context.get('user', {})
         del_user = users.get(sess, username=username)
         users.delete(sess, actioning_user=user, user=del_user)
@@ -64,7 +68,28 @@ class UserResource():
         resp.status = falcon.HTTP_200
 
 
-# class SessionResource():
-#     """Handlers for /api/v1/users/sessions endpoint."""
+class SessionResource():
+    """Handlers for /api/v1/users/sessions endpoint."""
 
-#     def on_get(self, req, resp):
+    def on_post(self, req, resp):
+        """Used for creating a new session or "logging in"."""
+        login = json.load(req.stream)
+        user = users.get(db(), username=login['username'])
+        if user is None:
+            resp.body = json.dumps({
+                'message': 'no user with that username exists.'
+            })
+            resp.status = falcon.HTTP_404
+        elif users.check_pw(user, login['password']):
+            sess_id = sessions.gen_session_id()
+            sessions.set(sess_id, user)
+            resp.set_cookie('PRAE_SESSION', sess_id,
+                            expires=datetime.now()+timedelta(hours=1))
+            resp.status = falcon.HTTP_200
+        else:
+            resp.body = json.dumps({'message': 'invalid password'})
+            resp.status = falcon.HTTP_401
+
+    def on_delete(self, req, resp):
+        """Used for invalidating sessions or "logging out"."""
+        sessions.delete(req.context.get('session_id'))
