b86292e8d778d4a394d33b0d4fb51af9d4f5dde0
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-04-20 16:18:58 -0400

    write project schemas

---
+++ src/praelatus/api/schemas/project.py
@@ -0,0 +1,18 @@
+"""Defines schema for project objects."""
+
+from praelatus.api.schemas.base import BaseSchema
+from praelatus.api.schemas.user import UserSchema
+
+
+class ProjectSchema(BaseSchema):
+    """Used for validation of project objects."""
+    schema = {
+        'type': 'object',
+        'properties': {
+            'id': {'type': 'integer'},
+            'name': {'type': 'string'},
+            'description': {'type': 'string'},
+            'key': {'type': 'string'},
+            'lead': UserSchema.schema
+        }
+    }
