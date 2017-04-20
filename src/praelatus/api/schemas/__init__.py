000577d51a8ed2d2385c8c46026626e688458dad
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-04-20 16:19:03 -0400

    re export schemas

--- src/praelatus/api/schemas/__init__.py
+++ src/praelatus/api/schemas/__init__.py
@@ -1,5 +1,10 @@
 """Re-export schema classes."""
 from praelatus.api.schemas.user import UserSchema
 from praelatus.api.schemas.user import SignupSchema
-from praelatus.api.schemas.ticket import StatusSchema
+from praelatus.api.schemas.workflow import StatusSchema
+from praelatus.api.schemas.workflow import TransitionSchema
+from praelatus.api.schemas.workflow import WorkflowSchema
 from praelatus.api.schemas.ticket import TicketSchema
+from praelatus.api.schemas.ticket import CommentSchema
+from praelatus.api.schemas.ticket import TicketTypeSchema
+from praelatus.api.schemas.project import ProjectSchema
