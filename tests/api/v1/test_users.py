b32550021731069bb97102412a4f31deaaae1464
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-04-21 13:56:28 -0400

    write CRUD API tests for users endpoints

--- tests/api/v1/test_users.py
+++ tests/api/v1/test_users.py
@@ -2,7 +2,8 @@ import falcon
 from praelatus.api.schemas import UserSchema
 
 
-def test_signup(client, headers):
+def test_crud_user_endpoints(client, headers):
+    # Sign up a new user
     new_user = {
         "username": "some_new_user",
         "password": "supersecure",
@@ -11,7 +12,37 @@ def test_signup(client, headers):
     }
 
     resp = client.post('/api/v1/users', new_user, headers=headers)
-    print(resp.body)
     assert resp.status == falcon.HTTP_200
     assert resp.json['token'] is not None
     UserSchema.validate(resp.json['user'])
+    new_user = resp.json['user']
+
+    # Try to log in to our new user
+    login = {
+        "username": "some_new_user",
+        "password": "supersecure"
+    }
+
+    resp = client.post('/api/v1/users/sessions', login, headers=headers)
+    assert resp.status == falcon.HTTP_200
+    assert resp.json['token'] is not None
+    token = resp.json['token']
+    UserSchema.validate(resp.json['user'])
+
+    # Get the new user
+    resp = client.get('/api/v1/users/some_new_user', headers=headers)
+    assert resp.status == falcon.HTTP_200
+    UserSchema.validate(resp.json)
+
+    auth_headers = headers
+    auth_headers['Authorization'] = 'Token ' + token
+
+    # Update the new user
+    new_user['username'] = 'delete_me'
+    resp = client.put('/api/v1/users/some_new_user', new_user,
+                      headers=auth_headers)
+    assert resp.json['message'] == 'Successfully updated user.'
+
+    # Delete the new user
+    resp = client.delete('/api/v1/users/delete_me', headers=auth_headers)
+    assert resp.json['message'] == 'Successfully deleted user.'
