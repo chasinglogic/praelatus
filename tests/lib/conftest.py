66ed203195dc6547fe2eb7c612f4cb44a6f7c124
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-04-21 13:56:42 -0400

    change to return jSON

--- tests/lib/conftest.py
+++ tests/lib/conftest.py
@@ -7,7 +7,7 @@ import praelatus.lib.users as users
 
 @pytest.fixture(scope='module')
 def admin():
-    return users.get(session(), username='testadmin')
+    return users.get(session(), username='testadmin').clean_dict()
 
 
 @pytest.fixture(scope='module')
