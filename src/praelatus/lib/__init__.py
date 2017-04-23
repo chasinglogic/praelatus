29b2c802c81e845d789ea0ac35146a90f50f2874
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-04-23 11:49:33 -0400

    make cleaner

--- src/praelatus/lib/__init__.py
+++ src/praelatus/lib/__init__.py
@@ -10,31 +10,32 @@ the function and they default to None or "Anonymous"
 """
 
 from praelatus.config import config
-from praelatus.models import Base
+from contextlib import contextmanager
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-def __init_db():
-    """Connect to the database and make the engine globally available."""
-    global engine
-    engine = create_engine(config.db_url)
-    Base.metadata.create_all(engine)
-    global Session
-    Session = sessionmaker(bind=engine)
+global engine
+engine = create_engine(config.db_url)
+
+global Session
+Session = sessionmaker(bind=engine)
 
 
+@contextmanager
 def session():
     """Return a session to for the database will connect if not connected."""
+    session = Session()
     try:
-        return Session()
-    except NameError:
-        __init_db()
-        return Session()
+        yield session
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
 
 
 def clean_db():
     """Remove all tables and data from the database."""
-    __init_db()
     engine.execute("DROP SCHEMA public CASCADE;")
     engine.execute("CREATE SCHEMA public;")
