2eafdd981263d8f922d5afcfa485268ca5ac50cf
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-04-23 20:35:56 -0400

    add new routes

--- src/praelatus/api/v1/__init__.py
+++ src/praelatus/api/v1/__init__.py
@@ -2,13 +2,16 @@
 
 from praelatus.api.v1.users import UsersResource
 from praelatus.api.v1.users import UserResource
-from praelatus.api.v1.users import SessionResource
+from praelatus.api.v1.users import TokensResource
 from praelatus.api.v1.tickets import TicketsResource
+from praelatus.api.v1.tickets import TicketResource
 
 
 def add_v1_routes(app, prefix='/api/v1/'):
     """Add all of the v1 routes to the given app."""
-    app.add_route(prefix+'users', UsersResource(SessionResource.create_session))
-    app.add_route(prefix+'users/{username}', UserResource())
-    app.add_route(prefix+'users/sessions', SessionResource())
-    app.add_route(prefix+'tickets', TicketsResource())
+    app.add_route(prefix + 'users',
+                  UsersResource(TokensResource.create_token))
+    app.add_route(prefix + 'users/{username}', UserResource())
+    app.add_route(prefix + 'tokens', TokensResource())
+    app.add_route(prefix + 'tickets', TicketsResource())
+    app.add_route(prefix + 'tickets/{key}', TicketResource())
