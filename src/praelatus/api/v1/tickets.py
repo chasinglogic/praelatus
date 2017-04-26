52c60051962c3902fe3fb26d87ec38ccdc41d05f
Mathew Robinson <chasinglogic@gmail.com>
2017-04-26 14:15:25 -0400

    write comment routes

--- src/praelatus/api/v1/tickets.py
+++ src/praelatus/api/v1/tickets.py
@@ -5,7 +5,9 @@ import falcon
 
 from praelatus.lib import session
 import praelatus.lib.tickets as tickets
+import praelatus.lib.projects as projects
 from praelatus.api.schemas import TicketSchema
+from praelatus.api.schemas import CommentSchema
 
 
 class TicketsResource():
@@ -29,7 +31,7 @@ class TicketsResource():
     def on_get(self, req, resp):
         """
         Return all tickets the current user has access to.
-        
+
         Accepts the query parameter filter which searches through
         tickets on the instance. This will eventually be replaced with
         a query language but for now supports simple text searching.
@@ -46,9 +48,9 @@ class TicketsResource():
 
 
 class TicketResource():
-    """Handlers for /api/v1/tickets/{key} endpoint."""
+    """Handlers for /api/v1/tickets/{ticket_key} endpoint."""
 
-    def on_get(self, req, resp, key):
+    def on_get(self, req, resp, ticket_key):
         """
         Retrieve a single ticket by ticket key.
 
@@ -57,10 +59,10 @@ class TicketResource():
         """
         user = req.context['user']
         with session() as db:
-            db_res = tickets.get(db, actioning_user=user, key=key)
+            db_res = tickets.get(db, actioning_user=user, key=ticket_key)
             resp.body = db_res.to_json()
 
-    def on_put(self, req, resp, key):
+    def on_put(self, req, resp, ticket_key):
         """
         Update the ticket identified by ticket_key.
 
@@ -71,13 +73,13 @@ class TicketResource():
         jsn = json.loads(req.bounded_stream.read().decode('utf-8'))
         TicketSchema.validate(jsn)
         with session() as db:
-            orig_tick = tickets.get(db, key=key)
+            orig_tick = tickets.get(db, key=ticket_key)
             if orig_tick is None:
                 raise falcon.HTTPNotFound()
             tickets.update(db, actioning_user=user,
                            orig_ticket=orig_tick, ticket=jsn)
 
-    def on_delete(self, req, resp, key):
+    def on_delete(self, req, resp, ticket_key):
         """
         Delete the ticket identified by ticket_key.
 
@@ -86,9 +88,93 @@ class TicketResource():
         """
         user = req.context['user']
         with session() as db:
-            tick = tickets.get(db, actioning_user=user, key=key)
+            tick = tickets.get(db, actioning_user=user, key=ticket_key)
             if tick is None:
                 raise falcon.HTTPNotFound()
             tickets.delete(db, actioning_user=user,
                            project=tick.project, ticket=tick)
         resp.body = json.dumps({'message': 'Successfully deleted ticket.'})
+
+
+class CommentsResource():
+    """Handlers for /api/v1/tickets/{ticket_key}/comments endpoint."""
+
+    def on_get(self, req, resp, ticket_key):
+        """
+        Retrieve all comments for the ticket indentified by ticket_key.
+
+        API Documentation:
+        https://docs.praelatus.io/API/Reference/#get-ticketsticket_key-comments
+        """
+        user = req.context['user']
+        with session() as db:
+            ticket = tickets.get(db, actioning_user=user, key=ticket_key)
+            comments = tickets.get_comments(db, actioning_user=user,
+                                            project=ticket.project,
+                                            ticket_key=ticket_key)
+            resp.body = json.dumps([x.clean_dict() for x in comments])
+
+    def on_post(self, req, resp, ticket_key):
+        """
+        Create a new comment for the ticket identified by ticket_key.
+
+        API Documentation:
+        https://docs.praelatus.io/API/Reference/#post-ticketsticket_key-comments
+        """
+        user = req.context['user']
+        jsn = json.loads(req.bounded_stream.read().decode('utf-8'))
+        if jsn.get('author') is None:
+            jsn['author'] = user
+        CommentSchema.validate(jsn)
+        with session() as db:
+            ticket = tickets.get(db, actioning_user=user, key=ticket_key)
+            comment = tickets.add_comment(db, actioning_user=user,
+                                          project=ticket.project,
+                                          ticket_id=ticket.id,
+                                          **jsn)
+            resp.body = comment.to_json()
+
+
+class CommentResource():
+    """Handlers for /api/v1/tickets/{ticket_key}/comments/{id} endpoint."""
+
+    def on_put(self, req, resp, ticket_key, id):
+        """
+        Update the comment at ID for ticket_key.
+
+        API Documentation:
+        https://docs.praelatus.io/API/Reference/#put-ticketsticket_key-commentsid
+        """
+        user = req.context['user']
+        jsn = json.loads(req.bounded_stream.read().decode('utf-8'))
+        CommentSchema.validate(jsn)
+        with session() as db:
+            project = projects.get(db, actioning_user=user,
+                                   key=ticket_key.split('-')[0])
+            ticket = tickets.get(db, actioning_user=user,
+                                 key=ticket_key, project=project)
+            comment = tickets.get_comment(db, int(id))
+            comment.body = jsn['body']
+            tickets.update_comment(db, comment, actioning_user=user,
+                                   project=ticket.project)
+
+            resp.body = comment.to_json()
+
+    def on_delete(self, req, resp, ticket_key, id):
+        """
+        Delete the comment at ID for ticket_key.
+
+        API Documentation:
+        https://docs.praelatus.io/API/Reference/#delete-ticketsticket_key-commentsid
+        """
+        user = req.context['user']
+        with session() as db:
+            project = projects.get(db, actioning_user=user,
+                                   key=ticket_key.split('-')[0])
+            ticket = tickets.get(db, actioning_user=user,
+                                 key=ticket_key, project=project)
+            comment = ticket.get_comment(db, int(id))
+            tickets.delete_comment(db, comment, actioning_user=user,
+                                   project=ticket.project)
+
+            resp.body = json.dumps({'message': 'successfully deleted comment'})
