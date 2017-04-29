edb2daa460e0ba0684d1bbde965b4eef1d4d58eb
Mathew Robinson <chasinglogic@gmail.com>
2017-04-28 22:43:51 -0400

    register all the basic CRUD routes

--- src/praelatus/api/v1/__init__.py
+++ src/praelatus/api/v1/__init__.py
@@ -9,6 +9,16 @@ from praelatus.api.v1.tickets import CommentsResource
 from praelatus.api.v1.tickets import CommentResource
 from praelatus.api.v1.projects import ProjectsResource
 from praelatus.api.v1.projects import ProjectResource
+from praelatus.api.v1.ticket_types import TicketTypesResource
+from praelatus.api.v1.ticket_types import TicketTypeResource
+from praelatus.api.v1.statuses import StatusesResource
+from praelatus.api.v1.statuses import StatusResource
+from praelatus.api.v1.fields import FieldsResource
+from praelatus.api.v1.fields import FieldResource
+from praelatus.api.v1.labels import LabelsResource
+from praelatus.api.v1.labels import LabelResource
+from praelatus.api.v1.workflows import WorkflowsResource
+from praelatus.api.v1.workflows import WorkflowResource
 
 
 def add_v1_routes(app, prefix='/api/v1/'):
@@ -17,6 +27,8 @@ def add_v1_routes(app, prefix='/api/v1/'):
     app.add_route(prefix + 'users',
                   UsersResource(TokensResource.create_token))
     app.add_route(prefix + 'users/{username}', UserResource())
+
+    # Tokens
     app.add_route(prefix + 'tokens', TokensResource())
 
     # Tickets
@@ -24,10 +36,31 @@ def add_v1_routes(app, prefix='/api/v1/'):
     app.add_route(prefix + 'tickets/{ticket_key}', TicketResource())
 
     # Comments
-    app.add_route(prefix + 'tickets/{ticket_key}/comments', CommentsResource())
+    app.add_route(prefix + 'tickets/{ticket_key}/comments',
+                  CommentsResource())
     app.add_route(prefix + 'tickets/{ticket_key}/comments/{id}',
                   CommentResource())
 
     # Projects
     app.add_route(prefix + 'projects', ProjectsResource())
     app.add_route(prefix + 'projects/{key}', ProjectResource())
+
+    # TicketTypes
+    app.add_route(prefix + 'ticketTypes', TicketTypesResource())
+    app.add_route(prefix + 'ticketTypes/{id}', TicketTypeResource())
+
+    # Statuses
+    app.add_route(prefix + 'statuses', StatusesResource())
+    app.add_route(prefix + 'statuses/{id}', StatusResource())
+
+    # Labels
+    app.add_route(prefix + 'labels', LabelsResource())
+    app.add_route(prefix + 'labels/{id}', LabelResource())
+
+    # Fields
+    app.add_route(prefix + 'fields', FieldsResource())
+    app.add_route(prefix + 'fields/{id}', FieldResource())
+
+    # Workflows
+    app.add_route(prefix + 'workflows', WorkflowsResource())
+    app.add_route(prefix + 'workflows/{id}', WorkflowResource())
