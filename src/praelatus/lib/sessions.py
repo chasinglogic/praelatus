ac8677aba7321ffcdfe1b6cca964942e125c82be
Mathew Robinson <chasinglogic@gmail.com>
2017-05-01 19:20:04 -0400

    Added key encoding

--- src/praelatus/lib/sessions.py
+++ src/praelatus/lib/sessions.py
@@ -1,20 +1,39 @@
+"""Manage authentication tokens serialization and deserialization."""
+
 import json
-import binascii
 import base64
+
+import praelatus.file_store as file_store
+
+from random import choice
+from string import ascii_uppercase
 from itsdangerous import TimestampSigner
 
 
 def get(signed_token):
     """Desearialize token into a user object."""
-    print("Unsigning Token")
-    token = serializer.unsign(signed_token, max_age=28800)
-    return json.loads(token)
+    # Base64 in python is picky so make it bytes first
+    bytes_token = signed_token.encode('ascii')
+    signed_token = base64.b64decode(bytes_token)
+    user = serializer.unsign(signed_token, max_age=28800)
+    return json.loads(user)
 
 
 def gen_session_id(user):
     """Generate a secure token."""
     jsn = json.dumps(user)
-    return serializer.sign(jsn)
+    return base64.b64encode(serializer.sign(jsn)).decode('ascii')
 
 
-serializer = TimestampSigner('test')
+def get_secret_key():
+    """Load the secret key if one exists else generate a new one."""
+    with file_store.get_file('session-key') as f:
+        if f:
+            return f.read()
+    key = ''.join(choice(ascii_uppercase) for i in range(64))
+    file_store.save_file('session-key', key)
+    return key
+
+
+secret_key = get_secret_key()
+serializer = TimestampSigner(secret_key)
