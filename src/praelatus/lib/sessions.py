fec619aba5f4a3139afb0539d89aeb9f08ea019d
Mathew Robinson <chasinglogic@gmail.com>
2017-05-02 08:58:10 -0400

    better and backwards compatible security, taken from django

--- src/praelatus/lib/sessions.py
+++ src/praelatus/lib/sessions.py
@@ -5,8 +5,34 @@ import base64
 
 import praelatus.file_store as file_store
 
-from secrets import token_hex
+from random import SystemRandom
 from itsdangerous import TimestampSigner
+
+random = SystemRandom()
+
+
+def get_random_string(length=12,
+                      allowed_chars='abcdefghijklmnopqrstuvwxyz'
+                                    'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'):
+    """
+    Return a securely generated random string.
+
+    The default length of 12 with the a-z, A-Z, 0-9 character set returns
+    a 71-bit value. log_2((26+26+10)^12) =~ 71 bits.
+
+    Taken from the django.utils.crypto module.
+    """
+    return ''.join(random.choice(allowed_chars) for i in range(length))
+
+
+def generate_secret_key():
+    """
+    Create a random secret key.
+
+    Taken from the Django project.
+    """
+    chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+    return get_random_string(50, chars)
 
 
 def get(signed_token):
@@ -29,7 +55,7 @@ def get_secret_key():
     with file_store.get_file('session-key') as f:
         if f:
             return f.read()
-    key = token_hex(32)
+    key = generate_secret_key()
     file_store.save_file('session-key', key)
     return key
 
