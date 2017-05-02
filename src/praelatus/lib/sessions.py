e96c659572f1f76205b90d69ba28612719e7ed22
Mathew Robinson <chasinglogic@gmail.com>
2017-05-02 08:44:15 -0400

    change to use secrets and utf-8

--- src/praelatus/lib/sessions.py
+++ src/praelatus/lib/sessions.py
@@ -5,24 +5,23 @@ import base64
 
 import praelatus.file_store as file_store
 
-from random import choice
-from string import ascii_uppercase
+from secrets import token_hex
 from itsdangerous import TimestampSigner
 
 
 def get(signed_token):
     """Desearialize token into a user object."""
     # Base64 in python is picky so make it bytes first
-    bytes_token = signed_token.encode('ascii')
+    bytes_token = signed_token.encode('utf-8')
     signed_token = base64.b64decode(bytes_token)
     user = serializer.unsign(signed_token, max_age=28800)
-    return json.loads(user)
+    return json.loads(user.decode('utf-8'))
 
 
 def gen_session_id(user):
     """Generate a secure token."""
     jsn = json.dumps(user)
-    return base64.b64encode(serializer.sign(jsn)).decode('ascii')
+    return base64.b64encode(serializer.sign(jsn)).decode('utf-8')
 
 
 def get_secret_key():
@@ -30,7 +29,7 @@ def get_secret_key():
     with file_store.get_file('session-key') as f:
         if f:
             return f.read()
-    key = ''.join(choice(ascii_uppercase) for i in range(64))
+    key = token_hex(32)
     file_store.save_file('session-key', key)
     return key
 
