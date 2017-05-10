87f7e249e5394f9a3979ca4130ba2ed1e9bafce6
Mathew Robinson <chasinglogic@gmail.com>
2017-05-10 19:43:10 -0400

    write project store

---
+++ src/praelatus/store/projects.py
@@ -0,0 +1,60 @@
+"""Contains definition for the ProjectStore class."""
+
+from sqlalchemy import or_
+
+from praelatus.models import Project
+from praelatus.models import User
+from praelatus.lib.permissions import permission_required
+from praelatus.lib.permissions import add_permission_query
+from praelatus.store.store import Store
+
+
+class ProjectStore(Store):
+    """Stores and retrieves Projects."""
+    model = Project
+
+    def get(self, db, uid=None, id=None, name=None,
+            actioning_user=None, **kwargs):
+        """Get a single project from the database."""
+        query = db.query(Project)
+        query = add_permission_query(db, query,
+                                     actioning_user, 'VIEW_PROJECT')
+        if uid is not None:
+            query = query.filter(Project.key == uid)
+        elif id is not None:
+            query = query.filter(Project.id == id)
+        elif name is not None:
+            query = query.filter(Project.name == name)
+        else:
+            return None
+        return query.first()
+
+    def search(self, db, search, actioning_user=None, **kwargs):
+        """Search through projects and return all matches."""
+        pattern = filter.replace('*', '%')
+        query = db.query(Project).filter(
+            or_(
+                Project.name.like(pattern),
+                Project.key.like(pattern),
+                User.username.like(pattern),
+                User.full_name.like(pattern)
+            )
+        )
+
+        query = add_permission_query(db, query, actioning_user, 'VIEW_PROJECT')
+        return query.order_by(Project.key).all()
+
+    @sys_admin_required
+    def new(self, db, **kwargs):
+
+    @permission_required('ADMIN_PROJECT')
+    def update(self, db, model=None, **kwargs):
+        """Update the project in the database."""
+        db.add(model)
+        db.commit()
+
+    @permission_required('ADMIN_PROJECT')
+    def delete(self, db, model=None, **kwargs):
+        """Update the project in the database."""
+        db.delete(model)
+        db.commit()
