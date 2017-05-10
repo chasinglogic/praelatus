0ab12628d03a25ef81e39d8aebae869477b2cc26
Mathew Robinson <chasinglogic@gmail.com>
2017-05-10 19:44:59 -0400

    seperate store class to prevent circular dependency

--- src/praelatus/store/__init__.py
+++ src/praelatus/store/__init__.py
@@ -1,43 +1,18 @@
 """Contains storage classes for Praelatus."""
 
+from praelatus.models import Role
+from praelatus.models import Status
+from praelatus.models import Field
+from praelatus.models import TicketType
+from praelatus.store.store import Store
+from praelatus.store.tickets import TicketStore  # noqa: F401
+from praelatus.store.comments import CommentStore  # noqa: F401
+from praelatus.store.permission_schemes import PermissionSchemeStore  # noqa: E501,F401
+from praelatus.store.workflows import WorkflowStore  # noqa: F401
+from praelatus.store.projects import ProjectStore  # noqa: F401
+from praelatus.store.labels import LabelStore  # noqa: F401
 
-class Store:
-    """A store manages storage of models.
-
-    This is the base class all other stores should inherit from.
-    """
-
-    def __init__(self, model):
-        """Model is the class that this store is used for."""
-        self.model = model
-
-    def get(self, db, uid=None, name=None, **kwargs):
-        """Get a single model, db is a SQL Alchemy session."""
-        query = db.query(self.model)
-
-        if uid is not None:
-            query = query.filter(self.model.id == uid)
-
-        if name is not None:
-            query = query.filter(self.model.name == name)
-
-        return query.first()
-
-    def search(self, db, search, **kwargs):
-        """Search through the models and return all matches."""
-        pattern = search.replace('*', '%')
-        return db.query(self.model).\
-            filter(self.model.name.like(pattern)).\
-            order_by(self.model.name).\
-            all()
-
-    def new(self, db, **kwargs):
-        """Create a new model in the database."""
-        new_model = self.model(**kwargs)
-        db.add(new_model)
-        db.commit()
-
-    def update(self, db, model=None, **kwargs):
-        """Update the model in the database."""
-        db.add(model)
-        db.commit()
+TicketTypeStore = Store(TicketType)
+StatusStore = Store(Status)
+RoleStore = Store(Role)
+FieldStore = Store(Field)
