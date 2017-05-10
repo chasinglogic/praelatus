7cf468da8118158c602bc201575e3c2e92644930
Mathew Robinson <chasinglogic@gmail.com>
2017-05-10 19:44:59 -0400

    seperate store class to prevent circular dependency

---
+++ src/praelatus/store/store.py
@@ -0,0 +1,53 @@
+"""Contains definition of the basic Store class."""
+
+from praelatus.lib.permissions import sys_admin_required
+
+
+class Store:
+    """A store manages storage of models.
+
+    This is the base class all other stores should inherit from.
+    """
+
+    def __init__(self, model):
+        """Model is the class that this store is used for."""
+        self.model = model
+
+    def get(self, db, uid=None, name=None, **kwargs):
+        """Get a single model, db is a SQL Alchemy session."""
+        query = db.query(self.model)
+
+        if uid is not None:
+            query = query.filter(self.model.id == uid)
+
+        if name is not None:
+            query = query.filter(self.model.name == name)
+
+        return query.first()
+
+    def search(self, db, search, **kwargs):
+        """Search through the models and return all matches."""
+        pattern = search.replace('*', '%')
+        return db.query(self.model).\
+            filter(self.model.name.like(pattern)).\
+            order_by(self.model.name).\
+            all()
+
+    @sys_admin_required
+    def new(self, db, **kwargs):
+        """Create a new model in the database."""
+        new_model = self.model(**kwargs)
+        db.add(new_model)
+        db.commit()
+
+    @sys_admin_required
+    def update(self, db, model=None, **kwargs):
+        """Update the model in the database."""
+        db.add(model)
+        db.commit()
+
+    @sys_admin_required
+    def delete(self, db, model=None, **kwargs):
+        """Update the model in the database."""
+        db.delete(model)
+        db.commit()
