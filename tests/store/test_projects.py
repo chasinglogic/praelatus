01c050694374518afe40cca36805cc2974d5a645
Mathew Robinson <chasinglogic@gmail.com>
2017-05-13 10:08:51 -0400

    start writing store tests

---
+++ tests/store/test_projects.py
@@ -0,0 +1,72 @@
+import praelatus.lib.users as users
+import praelatus.lib.projects as projects
+
+
+def test_get_one(db):
+    project = projects.get(db, key='TEST')
+    assert project is not None
+    assert project.key == 'TEST'
+
+
+def test_get_filter(db):
+    pjs = projects.get(db, filter='test*')
+    print(pjs)
+    assert pjs is not None
+    assert len(pjs) > 0
+    assert ('TEST' in pjs[0].key or 'test' in pjs[0].name.lower())
+
+
+def test_get_filter_action(db, admin):
+    pjs = projects.get(db, filter='test*', actioning_user=admin)
+    print(pjs)
+    assert pjs is not None
+    assert len(pjs) > 0
+    assert ('TEST' in pjs[0].key or 'test' in pjs[0].name.lower())
+
+
+def test_update(db):
+    new_name = 'Super Duper Test Project'
+    lead = users.get(db, username='testuser')
+    project = projects.get(db, key='TEST')
+    project.name = new_name
+
+    projects.update(db, project=project, actioning_user=lead)
+
+    proj = projects.get(db, id=project.id)
+    assert proj is not None
+    assert proj.name == new_name
+
+
+def test_delete(db, admin):
+    lead = users.get(db, username='testuser')
+    project = {
+        'name': 'DELETE THIS PROJECT',
+        'key':  'DELETE',
+        'lead': {'id': lead.id}
+    }
+
+    projects.new(db, actioning_user=admin, **project)
+
+    p = projects.get(db, key='DELETE')
+    assert p is not None
+
+    projects.delete(db, project=p, actioning_user=lead)
+
+    p = projects.get(db, key='DELETE')
+    assert p is None
+
+
+def test_json(db, admin):
+    project_json = {
+        'name': 'JSON TEST Project',
+        'key':  'JSON',
+        'lead': admin,
+        'homepage': None,
+        'icon_url': None,
+        'repo': None
+    }
+
+    p = projects.new(db, actioning_user=admin, **project_json)
+    project_json['id'] = p.id
+    project_json['created_date'] = str(p.created_date)
+    assert p.clean_dict() == project_json
---
+++ tests/store/test_users.py
@@ -0,0 +1,59 @@
+import praelatus.lib.users as users
+
+from praelatus.api.schemas import SignupSchema
+from praelatus.api.schemas import UserSchema
+
+
+def test_get(db):
+    user = users.get(db, username='testadmin')
+    assert user is not None
+    assert user.username == 'testadmin'
+
+
+def test_get_with_filter(db):
+    usrs = users.get(db, filter='test*')
+    assert usrs is not None
+    assert len(usrs) > 0
+    assert usrs[0].username == 'testadmin'
+
+
+def test_delete(db):
+    users.new(db, **{
+        'username': 'delete_me',
+        'password': 'none',
+        'email': 'delete_me@delete.com',
+        'full_name': 'DELETE ME',
+        'is_active': True,
+    })
+
+    # verify we can retreieve the user
+    u = users.get(db, username='delete_me')
+    assert u is not None
+
+    users.delete(db, u, actioning_user=u.clean_dict())
+
+    u = users.get(db, username='delete_me')
+    assert u is None
+
+
+def test_update(db):
+    email = 'anonmyous@example.com'
+    anon = users.get(db, id=1)
+
+    anon.email = email
+
+    users.update(db, anon, actioning_user=anon.clean_dict())
+
+    a = users.get(db, id=1)
+    assert a is not None
+    assert a.email == email
+
+
+def test_gravatar():
+    gravatar = 'https://gravatar.com/avatar/9cd743073d56a5958655e3cf2105728b'
+    assert gravatar == users.gravatar('chasinglogic@gmail.com')
+
+
+def test_check_pw(db):
+    admin = users.get(db, username='testadmin')
+    assert users.check_pw(admin, 'test')
