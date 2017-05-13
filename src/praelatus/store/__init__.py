c03da6a953ca712555cb8559484aa984adacc842
Mathew Robinson <chasinglogic@gmail.com>
2017-05-13 10:08:43 -0400

    finish up store classes

--- src/praelatus/store/__init__.py
+++ src/praelatus/store/__init__.py
@@ -1,7 +1,6 @@
 """Contains storage classes for Praelatus."""
 
 from praelatus.models import Role
-from praelatus.models import Status
 from praelatus.models import Field
 from praelatus.models import TicketType
 from praelatus.store.store import Store
@@ -9,10 +8,10 @@ from praelatus.store.tickets import TicketStore  # noqa: F401
 from praelatus.store.comments import CommentStore  # noqa: F401
 from praelatus.store.permission_schemes import PermissionSchemeStore  # noqa: E501,F401
 from praelatus.store.workflows import WorkflowStore  # noqa: F401
+from praelatus.store.workflows import StatusStore  # noqa: F401
 from praelatus.store.projects import ProjectStore  # noqa: F401
 from praelatus.store.labels import LabelStore  # noqa: F401
 
 TicketTypeStore = Store(TicketType)
-StatusStore = Store(Status)
 RoleStore = Store(Role)
 FieldStore = Store(Field)
---
+++ src/praelatus/store/workflows.py
@@ -0,0 +1,71 @@
+"""Contains definition for the WorkflowStore."""
+
+from praelatus.store import Store
+from praelatus.models import Hook
+from praelatus.models import Status
+from praelatus.models import Workflow
+from praelatus.models import Transition
+from praelatus.lib.permissions import sys_admin_required
+
+StatusStore = Store(Status)
+
+
+class WorkflowStore(Store):
+    """Stores and retrieves workflows."""
+    model = Workflow
+    status_store = StatusStore
+
+    @sys_admin_required
+    def new(self, db, actioning_user=None, **kwargs):
+        """Add workflow to if acitoning_user has permission.
+
+        Required Keyword Arguments:
+        """
+        new_workflow = Workflow(
+            name=kwargs['name'],
+            description=kwargs.get('description')
+        )
+
+        db.add(new_workflow)
+
+        transitions = kwargs.get('transitions', {})
+        for from_status_name, available_transitions in transitions.items():
+            from_status = self.status_store.get(db, name=from_status_name)
+            if from_status is None and from_status_name != "Create":
+                from_status = self.status_store.new(
+                    db,
+                    actioning_user=actioning_user,
+                    name=from_status
+                )
+
+            for tran in available_transitions:
+                to_status = self.status_store.get(
+                    db,
+                    name=tran['to_status']['name']
+                )
+
+                if to_status is None:
+                    to_status = self.status_store.new(db, **to_status)
+
+                hks = []
+                for h in tran.get('hooks', []):
+                    hks.append(Hook(
+                        name=h['name'],
+                        description=h.get('description'),
+                        body=h.get('body'),
+                        method=h.get('method'),
+                        url=h.get('url')
+                    ))
+
+                new_tr = Transition(
+                    name=tran['name'],
+                    from_status=from_status,
+                    to_status=to_status,
+                    hooks=hks,
+                )
+
+                db.add(new_tr)
+                new_workflow.transitions.append(new_tr)
+
+        db.commit()
+        return new_workflow
