6090d19454cde35d7586b1b339683af4201e56b4
Mathew Robinson <chasinglogic@gmail.com>
2017-05-18 21:28:13 -0400

    create custom fieldstore
    Necessary to do extra processing on new around FieldOptions

--- src/praelatus/store/__init__.py
+++ src/praelatus/store/__init__.py
@@ -1,10 +1,10 @@
 """Contains storage classes for Praelatus."""
 
 from praelatus.models import Role
-from praelatus.models import Field
 from praelatus.models import TicketType
 from praelatus.store.store import Store
 from praelatus.store.users import store as UserStore
+from praelatus.store.fields import store as FieldStore
 from praelatus.store.tickets import store as TicketStore  # noqa: F401
 from praelatus.store.comments import store as CommentStore  # noqa: F401
 from praelatus.store.permission_schemes import store as PermissionSchemeStore  # noqa: E501,F401
@@ -15,4 +15,3 @@ from praelatus.store.labels import store as LabelStore  # noqa: F401
 
 TicketTypeStore = Store(TicketType)
 RoleStore = Store(Role)
-FieldStore = Store(Field)
