fc889f37974c9162bae20c068f01093d3829afc5
Mathew Robinson <chasinglogic@gmail.com>
2017-05-24 11:24:35 -0400

    log webhook responses

--- src/praelatus/events/web_hooks.py
+++ src/praelatus/events/web_hooks.py
@@ -3,27 +3,24 @@
 import requests
 import jinja2
 
+from celery.utils.log import get_task_logger
+
 from praelatus.events.app import app
+
+logger = get_task_logger(__name__)
 
 
 @app.task
-def fire_web_hooks(hooks, ticket):
+def send_web_hooks(hooks, ticket):
     """Fire all hooks as web hooks for ticket.
 
     Hooks is an array of hook dict objects and ticket is a ticket dict
     object. Both as returned by jsonify()
     """
-    results = []
     for h in hooks:
         b = jinja2.Template(h['body']).render(**ticket)
         r = requests.Request(h['method'], h['url'], data=b)
         with requests.Session() as s:
             res = s.send(r)
-            results.append({
-                'URL': h['url'],
-                'Status': res.status_code,
-                'Method': h['method'],
-                'RequestBody': b,
-                'ResponseBody': res.text
-            })
-    return results
+            logger.info("[WEBHOOKS] %s: %s StatusCode: %d Response: %s" %
+                        (h['method'], h['url'], res.status_code, res.text))
