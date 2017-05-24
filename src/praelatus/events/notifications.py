2b7b94d63778175d41453cce310afd89c5d0ba1e
Mathew Robinson <chasinglogic@gmail.com>
2017-05-23 21:35:00 -0400

    start testing email

--- src/praelatus/events/notifications.py
+++ src/praelatus/events/notifications.py
@@ -1,17 +1,28 @@
 import smtplib
+
+from jinja2 import Template
+from pkg_resources import resource_string
+from pkg_resources import resource_exists
 from email.mime.text import MIMEText
-import jinja2
 from praelatus.models.events import EventType
 from praelatus.models.events import Event
 from praelatus.models import *
 
-comment_template = jinja2.Template("""
-Your home slice {{ user.full_name }} commented on {{ ticket.key }}:
 
-{{ comment.body }}
-""")
+print(resource_exists('praelatus', 'templates/email/html/comment.html'))
+print(resource_string('praelatus', 'templates/email/html/comment.html'))
 
-transition_template = jinja2.Template("""
+templates = {
+    'html': {
+        'comment': Template(resource_string('praelatus', 'templates/email/html/comment.html')),
+        # 'transition': resource_string('praelatus', 'templates/email/html/transition.html')
+    },
+    'text': {}
+}
+
+mta = smtplib.SMTP('praelatus.io', 587)
+
+transition_template = Template("""
 {{ ticket.key }} has moved from {{ transition.from_status.name }} to
 {{ transition.to_status.name }}
 """)
@@ -23,7 +34,7 @@ def send_email(recipients, event):
 
     if event.event_type == EventType.COMMENT_ADDED:
         subject = '%s comment added' % (event.ticket.key)
-        body = comment_template.\
+        body = templates['html']['comment'].\
             render(ticket=event.ticket,
                    user=event.user,
                    comment=event.comment)
@@ -33,9 +44,10 @@ def send_email(recipients, event):
     msg = MIMEText(body)
     msg['Subject'] = subject
     msg['To'] = recipients
-    msg['From'] = 'Mat'
-
+    msg['From'] = 'mrobinson@praelatus.io'
     print(msg)
+    mta.send_message(msg)
+
 
 if __name__ == '__main__':
     u = User(**{
@@ -64,4 +76,4 @@ if __name__ == '__main__':
         )
     )
 
-    send_email('ryan.brzezinski867@gmail.com', e)
+    send_email('ryan.brzezinski867@gmail.com;mrobinson@praelatus.io', e)
