0fb6934e38ae7b1f725bab42ee92292e70e18f05
Mathew Robinson <chasinglogic@gmail.com>
2017-05-29 19:46:17 -0400

    don't rely on pkg_resources

--- src/praelatus/templates/__init__.py
+++ src/praelatus/templates/__init__.py
@@ -1,10 +1,10 @@
 """Functions for loading templates."""
 
+from os.path import join
 from flask import session
 from flask import render_template as flask_render_template
 from praelatus.config import config
 from praelatus import __version__
-from pkg_resources import resource_string
 from jinja2 import Template
 
 
@@ -13,13 +13,13 @@ def load_template(tmpl_name):
 
     tmpl_name is a path relative to the template directory.
     """
-    body = resource_string('praelatus', 'templates/' + tmpl_name)
-    return Template(body.decode('utf-8'))
+    with open(join('praelatus', 'templates', tmpl_name)) as t:
+        return Template(t)
 
 
 def render_template(tmpl_name, **kwargs):
     """A wrapper for flask render_template that adds some of our always available variables."""
     return flask_render_template(tmpl_name, config=config,
-                                 user=session.get('user'),
+                                 current_user=session.get('user'),
                                  prae_version=__version__,
                                  **kwargs)
