5868070541b38cdccd48076a4568047f3796b43a
Mathew Robinson <chasinglogic@gmail.com>
2017-06-01 17:07:20 -0400

    add some javascript for use in interactive parts of app

---
+++ praelatus/static/js/lib.js
@@ -0,0 +1,15 @@
+/* Common functions used in Praelatus JS */
+
+// Search a model returns a promise of the JSON response.
+function searchModel(model_name, search) {
+  console.log('sending','/api/v1/'+model_name+'?filter='+search);
+  return fetch('/api/v1/'+model_name+'?filter='+search)
+  .then(function(response) {
+    return response.json();
+  });
+}
+
+function getParameterByName(name) {
+    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
