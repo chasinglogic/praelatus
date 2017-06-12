1b39992963df9e0c00a500ba1452b549417f598a
Mathew Robinson <chasinglogic@gmail.com>
2017-06-11 23:23:07 -0400

    start work on using django-filter for tickets

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from django_filters.views import FilterView
 
 from . import views
+from . import models
 
 urlpatterns = [
     url(r'^(?P<key>[A-z-]{1,6}-[0-9]{1,})$', views.show, name='show'),
+    url(r'^search', FilterView.as_view(model=models.Ticket)),
 ]
