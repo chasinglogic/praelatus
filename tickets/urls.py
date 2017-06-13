8418c6b6afd47ff38b35f24c3ce60a0ec7276839
Mathew Robinson <chasinglogic@gmail.com>
2017-06-13 11:48:52 -0400

    Transitioning now works

--- tickets/urls.py
+++ tickets/urls.py
@@ -6,6 +6,7 @@ from . import models, views
 
 urlpatterns = [
     url(r'^(?P<key>[A-z-]{1,6}-[0-9]{1,})$', views.show, name='show'),
+    url(r'^(?P<key>[A-z-]{1,6}-[0-9]{1,})/transition$', views.transition, name='transition'),
     url(r'^search', FilterView.as_view(model=models.Ticket)),
     url(r'^dashboard', views.dashboard),
 ]
