8c11bec19d9fcfebc68ddb9c0b5d4b302686b206
Mathew Robinson <chasinglogic@gmail.com>
2017-06-19 18:20:11 -0400

    Fixed add_link route

--- tickets/urls.py
+++ tickets/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^(?P<key>[A-z-]{1,6}-[0-9]{1,})$', views.show, name='show'),
     url(r'^(?P<key>[A-z-]{1,6}-[0-9]{1,})/edit$', views.edit_ticket, name='edit'),
     url(r'^(?P<key>[A-z-]{1,6}-[0-9]{1,})/attachment$', views.attachments, name='attachment'),
+    url(r'^(?P<key>[A-z-]{1,6}-[0-9]{1,})/link$', views.add_link, name='add_link'),
     url(r'^create/?$', views.create_prompt, name='create_prompt'),
     url(r'^create/(?P<project_key>[A-z-]{1,6})/(?P<ticket_type>[A-z]+)$', views.create, name='create'),
     url(r'^search', FilterView.as_view(model=models.Ticket)),
@@ -18,5 +19,4 @@ urlpatterns = [
     url(r'^comments/(?P<id>[0-9]+)', views.edit_comment, name='edit_comment'),
 
     url(r'^dashboard', views.dashboard),
-    url(r'^add_link_form', views.add_link, name='add_link')
 ]
