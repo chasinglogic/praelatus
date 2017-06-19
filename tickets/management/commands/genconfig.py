2c1f5a8ea07aa5444b4f93959ecbf06bc3ed6cc8
Mathew Robinson <chasinglogic@gmail.com>
2017-06-19 11:52:13 -0400

    now support a yaml config file

---
+++ tickets/management/commands/genconfig.py
@@ -0,0 +1,23 @@
+import yaml
+from socket import gethostname
+from os.path import join
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Generate the default minimal config.'
+
+    def handle(self, *args, **kwargs):
+        config = {
+            'mq_server': settings.CELERY_BROKER_URL,
+            'allowed_hosts': [gethostname()],
+            'media_root': settings.MEDIA_ROOT,
+            'static_root': settings.STATIC_ROOT,
+            'language_code': settings.LANGUAGE_CODE,
+            'cache': settings.CACHES,
+            'database': settings.DATABASES
+        }
+
+        with open(join(settings.DATA_DIR, 'config.yaml'), 'w') as f:
+            yaml.dump(config, f, width=80, indent=4, default_flow_style=False)
