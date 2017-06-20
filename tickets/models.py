4b4f89f3d74474f6e16d1c29273d28fb073519b8
Mathew Robinson <chasinglogic@gmail.com>
2017-06-20 14:38:14 -0400

    Fixed small syntax error

--- tickets/models.py
+++ tickets/models.py
@@ -62,8 +62,7 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
-class Meta:
+    class Meta:
         ordering = ['created_at']
 
 
