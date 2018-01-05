2dfde3c062148e65bbc4af36fc1699b43f831785
Mathew Robinson <chasinglogic@gmail.com>
2018-01-04 22:35:57 -0500

    Move to webpack based static asset managment

---
+++ assets/js/index.js
@@ -0,0 +1,33 @@
+/* Common functions used in Praelatus JS */
+
+import $ from 'jquery';
+
+window.jQuery = $;
+window.$ = $;
+
+import SimpleMDE from 'simplemde';
+
+window.SimpleMDE = SimpleMDE;
+
+function getParameterByName(name) {
+    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
+
+
+// using jQuery
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
---
+++ webpack.config.js
@@ -0,0 +1,87 @@
+'use strict';
+
+var path = require('path');
+var glob = require('glob');
+var webpack = require('webpack');
+var extractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: [
+    'jquery',
+    'tether',
+    'popper.js',
+    'moment',
+    'select2',
+    'bootstrap',
+    'simplemde',
+    path.join(__dirname, '/assets/css/index.scss'),
+    path.join(__dirname, '/assets/favicon.ico'),
+    path.join(__dirname, '/assets/js/index.js')
+  ].concat(glob.sync('./assets/img/*')),
+  output: {
+    path: path.join(__dirname, '/static/'),
+    filename: 'index.js',
+    publicPath: '/'
+  },
+  plugins: [
+    /* Assign the module and chunk ids by occurrence count. Ids that are used
+       often get lower (shorter) ids. This make ids predictable reduces total file
+       size and is recommended. */
+    new webpack.optimize.OccurrenceOrderPlugin(),
+
+    /* Seperate sass styles into a css file */
+    new extractTextPlugin('index.css'),
+
+    /* Enables Hot Module Replacement. (This requires records data if not in
+       dev-server mode, recordsPath) */
+    new webpack.HotModuleReplacementPlugin(),
+
+    /* When there are errors while compiling this plugin skips the emitting
+       phase (and recording phase), so there are no assets emitted that include
+       errors. The emitted flag in the stats is false for all assets. If you are
+       using the CLI, the webpack process will not exit with an error code by
+       enabling this plugin. If you want webpack to "fail" when using the CLI,
+       please check out the bail option. */
+    new webpack.NoEmitOnErrorsPlugin(),
+
+    /* Provide jQuery in the way that bootstrap expects */
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      Tether: 'tether',
+      Popper: 'popper.js',
+      SimpleMDE: 'simplemde'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        loader: 'css-loader'
+      },
+      {
+        test: /\.s(c|a)ss$/,
+        use: extractTextPlugin.extract({ use: [
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]})
+      },
+      {
+        test: /\.(eot|ttf|woff2?|svg|ico)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]'
+            }
+          }
+        ]
+      },
+    ]
+  }
+};
